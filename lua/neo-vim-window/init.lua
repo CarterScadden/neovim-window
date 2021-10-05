0996e2231a05c2d8b19b195bec9184d12c72658b
Carter_E_Scadden <carter@srtlabs.com>
2021-10-05 09:08:22 -0600

    started on making window dynamic

--- lua/neo-vim-window/init.lua
+++ lua/neo-vim-window/init.lua
@@ -1,12 +1,27 @@
-print("Reading Lua File - Carter")
+local PADDING = 10
+local NeoVimWindow = {}
 local buffer, window
 
-local function Start()
-  print("running start!");
+function NeoVimWindow:New(content, padding)
+  local obj = {
+    padding = padding or PADDING,
+    content = width or 15,
+    _width = 0,
+    _height = 0,
+    _buffer = vim.api.nvim_create_buf(false, true),
+    _window = nil
+  }
 
+  vim.api.nvim_buf_set_text(obj._buffer, 0, 0, 20, 20, {})
+
+  self.__index = self;
+  return setmetatable(obj, self)
+end
+
+
+local function Start()
   local uiState = vim.api.nvim_list_uis()[1]
   print("Window Size!", uiState.width, uiState.height)
-  --[[
   buffer = vim.api.nvim_create_buf(false, true) -- create empty buffer
   window = vim.api.nvim_open_win(buffer, true, {
     relative="editor",
@@ -15,8 +30,9 @@ local function Start()
     col = 2,
     row = 2,
   })
-  --]]
 end
+
+vim.api.nvim_hide_win()
 
 local function Resize()
   local uiStates = vim.api.nvim_list_uis()
